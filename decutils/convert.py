3ea8f3539bb6c4ea2efb59efadfa069c5fc93fc6
motty.mio2 <motty.mio2@gmail.com>
2021-04-30 01:59:04 +0900

    refactor fixed-point

--- decutils/convert.py
+++ decutils/convert.py
@@ -32,54 +32,59 @@ def binary2float(binary: str) -> float:
     return hex2float(hex(int(binary, 2)))
 
 
-def float2fix(x: int, IL: int, FL: int, header: bool = False, select: str = 'absolute', fraction: str = 'ceil') -> str:
-    '''
+def float2fix(x: int, IL: int, FL: int, header: bool = False, select: str = "absolute", fraction: str = "ceil") -> str:
+    """
     x：入力
     IL：integer length
     FL：deciminal length
     select：complement(2の補数表現) or absolute(絶対値表現, header=True を推奨)
     fraction：端数の処理 ceil(xより大きい最小の数値)，zero(0の方へ寄せる，正はfloor，負)
-    '''
+    """
     # 端数の処理
-    if fraction == 'ceil':
-        x = math.ceil(x*2**FL)
-    elif fraction == 'zero':
-        x = int(x*2**FL)
-    elif fraction == 'floor':
-        x = math.floor(x*2**FL)
+    if fraction == "ceil":
+        x = math.ceil(x * 2 ** FL)
+    elif fraction == "zero":
+        x = int(x * 2 ** FL)
+    elif fraction == "floor":
+        x = math.floor(x * 2 ** FL)
 
     # オーバー(アンダー)フロー
-    x = 2**(IL+FL-1)-1 if x >= 2**(IL+FL-1) else x
-    x = -1*2**(IL+FL-1)+1 if x <= -1*2**(IL+FL-1)+1 else x
+    if x >= (max_size := 2 ** (IL + FL - 1)):
+        x = max_size
+    elif x <= (min_size := -1 * max_size + 1):
+        x = min_size
 
-    # 2の補数
-    if (select == 'complement'):
+    if select == "complement":  # 2の補数
         # 絶対値
-        y = '0' + format(x, '0{}b'.format(IL+FL-1)) if x >= 0 else format(x & (2**(IL+FL)-1), '0{}b'.format(IL+FL))
+        y: str = "0" + format(x, f"0{IL + FL - 1}b") if x >= 0 else format(x & (2 ** (IL + FL) - 1), f"0{IL+FL}b")
         # header
-        y = '{}\'b'.format(IL+FL) + y if header else y
-​    # 正負記号 + 絶対値
-    # Verilogに直書きすると2の補数になります。
-    if (select == 'absolute'):
-        y = format(abs(x), '0{}b'.format(IL+FL-1))
-        if (header == True):
-            y = '-{}\'b'.format(IL+FL) + y if x <= 0 else '{}\'b'.format(IL+FL) + y
+        y = f"{IL+FL}'b" + y if header else y
+    elif select == "absolute":  # 正負記号 + 絶対値 Verilogに直書きすると2の補数になります。
+        y: str = format(abs(x), f"0{IL + FL - 1}b")
+        if header == True:
+            if x <= 0:
+                y = f"-{IL+FL}'b" + y
+            else:
+                y = f"{IL+FL}'b" + y
+    else:
+        raise Exception("Unknown Option")
 
     return y
-​
 
 
-def fix2float(x: str, FL: int, select: str='absolute') -> float:
-    '''
+def fix2float(x: str, FL: int, select: str = "absolute") -> float:
+    """
     x：入力
     FL：小数部分
     select：2の補数 or 絶対値
-    '''
-    # 2の補数
-    if (select == 'complement'):
+    Args :
+        select
+    """
+    if select == "complement":  # 2の補数
         y = -int(x[0]) << len(x) | int(x, 2)
-        y /= 2**FL
-    # 全て正の数と判断
-    if (select == 'absolute'):
-        y = int(x, 2) / 2**FL
+        y /= 2 ** FL
+    elif select == "absolute":  # 全て正の数と判断
+        y = int(x, 2) / 2 ** FL
+    else:
+        raise Exception("Unknown Option")
     return y
